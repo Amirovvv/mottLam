2434273e10a3b17ddde4ae2b08f723bad06d1b19
Khasan Amirov <58747052+Amirovvv@users.noreply.github.com>
2024-01-05 18:52:02 +0300

    chore: auto imports and cleaning

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+
+  plugins: [
+    Vue(),
+    AutoImport({
+      imports: [
+        'vue'
+      ],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      dts: 'src/components.d.ts',
+    }),
+  ],
 })
