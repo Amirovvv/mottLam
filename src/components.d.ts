7432d00d87b9505dfde0f7c4d700c5f5d70d428b
Khasan Amirov <58747052+Amirovvv@users.noreply.github.com>
2024-01-11 20:18:57 +0300

    feat: letter component

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    LetterCard: typeof import('./components/LetterCard.vue')['default']
     NavBar: typeof import('./components/NavBar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
---
+++ src/components/LetterCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="letter">
+    <span>буква</span>
+  </div>
+</template>
+
+<style scoped>
+.letter {
+  background: #ffffff;
+  height: 140px;
+  width: 108px;
+  padding: 10px 28px;
+  font-size: 60px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 6px 0px rgba(34, 60, 80, 0.2);
+  display: flex;
+  align-items: center;
+}
+</style>
