6fd194b2943a53833ff0818205adce19e781eb9e
Khasan Amirov <58747052+Amirovvv@users.noreply.github.com>
2024-01-17 20:47:08 +0300

    style: mod card letter

--- src/components/LetterCard.vue
+++ src/components/LetterCard.vue
@@ -4,22 +4,35 @@ defineProps(['letter'])
 
 <template>
   <div class="letter">
-    <span>{{ letter }}</span>
+    <div class="i-mdi-volume-high text-28px text-orange letter-icon" />
+    <div>{{ letter }}</div>
+    <div class="letter-more">подробнее</div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .letter {
   background: #ffffff;
-  height: 160px;
-  width: 108px;
-  padding: 10px 28px;
+  gap: 34px;
+  height: 216px;
+  width: 144px;
+  padding: 24px 28px;
   font-size: 48px;
   border-radius: 20px;
   box-shadow: 0px 0px 6px 0px rgba(34, 60, 80, 0.2);
   display: flex;
   align-items: center;
-  justify-content: center;
   font-family: monospace;
+  flex-direction: column;
+  justify-content: flex-start;
+
+  &-icon {
+    cursor: pointer;
+  }
+
+  &-more {
+    font-size: 14px;
+    visibility: hidden;
+  }
 }
 </style>
